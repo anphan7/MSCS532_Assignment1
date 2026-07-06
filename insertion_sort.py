f041f06f2ffd4021ad3a6245e23380ec62396b09
AnPhan <an.phan@elotouch.com>
2026-07-05 18:41:26 -0700

    Create insert sort algorithm and add test case runner

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,6 +1,25 @@
-"""Insertion Sort Algorithm - MSCS532 Assignment 1."""
+import sys
 
-# Starting point: the list of numbers we want to sort.
-numbers = [5, 2, 9, 1, 7, 3]
 
-print("Numbers to sort:", numbers)
+class InsertionSort:
+    def sort(self, arr):
+        for i in range(1, len(arr)):
+            key = arr[i]
+            j = i - 1
+            while j >= 0 and arr[j] < key:
+                arr[j + 1] = arr[j]
+                j -= 1
+            arr[j + 1] = key
+        return arr
+
+
+def main():
+    text = sys.stdin.read()
+    numbers = [int(x) for x in text.split(",")]
+    sortedNumbers = InsertionSort().sort(numbers)
+    result = ",".join(str(n) for n in sortedNumbers)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
